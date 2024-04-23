92ae866ba88090628851ca788c955671253abdf1
doogoa <zswlv55@gmail.com>
2024-04-24 00:37:20 +0800

    update import

---
+++ src/main/java/com/example/demo/service/SolutionService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.RoundUpList;
+
+public interface SolutionService {
+    int newSolution(int n);
+
+    void solution(RoundUpList roundUpList);
+}
