1ee0769278fdf8486f9915300e89626620de4e45
doogoa <zswlv55@gmail.com>
2025-06-20 20:29:32 +0800

    static

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -15,18 +15,21 @@ import java.util.List;
 @Slf4j
 @WriteLog
 public class HelloWorld {
+    private static int counter = 0;
+    
     private SolutionService solutionService;
     @Autowired
     private void setSolutionService(SolutionService solutionService){
         this.solutionService = solutionService;
     }
     @RequestMapping("/hello/world")
-    public String helloWorld() {
-        log.info("2023/04/14 create a new project in github");
+    // 使用 synchronized 保证线程安全
+    public synchronized String helloWorld() {
+        counter++;
         List<String> finalList = new ArrayList<>(List.of("Hello"));
         List<String> tempList = new ArrayList<>(List.of("World"));
 
-        return CommonUtil.addAll(finalList, tempList).toString();
+        return CommonUtil.addAll(finalList, tempList).toString() + " " + counter;
     }
 
     @PostMapping(value = "/hello/world")
